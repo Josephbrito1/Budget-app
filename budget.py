327676cf74cd0428562ae29df07af442321cae43
Josephbrito1 <107650135+Josephbrito1@users.noreply.github.com>
2022-09-07 18:00:36 -0400

    Update budget.py

--- budget.py
+++ budget.py
@@ -37,70 +37,22 @@ class Category:
         if (self.balance >= amount):
             return True
         return False 
-          
-food = Category("Food")
-print(food.deposit(1000, "initial deposit"))
-print(food.withdraw(10.15, "groceries"))
-print(food.withdraw(15.89, "restaurant and more food for dessert"))
-print(food.balance)
+#esto que esta aqui abajo solo son pruebas para ver si funcionaba la app           
+#food = Category("Food")
+#print(food.deposit(1000, "initial deposit"))
+#print(food.withdraw(10.15, "groceries"))
+#print(food.withdraw(15.89, "restaurant and more food for dessert"))
+#print(food.balance)
 
     
 def create_spend_chart(categories):
     
-    row="persentage spend by category\n"
+    column="persentage spend by category\n"
     for i in range(100, -1, -10):
-        row += f"{str(i)+'/':>4}\n"
+        column += f"{str(i)+'/':>4}\n"
         
-    print(row) 
+    print(column) 
     
 create_spend_chart([])       
     
-#   chart= "Percentage spent by category\n"
-#   percentList = []
-#   totalSpent= 0
- 
-#   #get percent
-#   for category in categories:
-#     wthdrAmount = -(category.ledger[1]["amount"])
-#     totalSpent += wthdrAmount
-#   for category in categories:
-#     wthdrAmount = -(category.ledger[1]["amount"])
-#     percentList.append(int((wthdrAmount/totalSpent)*100))
-
-#   #make bars
-#   top=100
-#   while top >= 0:
-#     i = 0
-#     if len(str(top)) < 3:
-#       while i < (3-len(str(top))):
-#         chart += (' ')
-#         i += 1
-#     chart = chart + str(top) + '|'
-#     i = 0
-#     while i < len(categories):
-#       if (percentList[i] >= top):
-#         chart += " o "
-#       else: chart += "   "
-#       i += 1
-#     chart += " \n"
-#     top -= 10
-#   #line
-#   chart = chart + "    " + ("---"* len(categories)) + "-"
-#   #categories
-#   maxLength= 0
-#   for category in categories:
-#     if len(category.name) > maxLength:
-#       maxLength = len(category.name)
-#   i = 0
-#   while i < maxLength:
-#     chart += "\n    "
-#     for category in categories:
-#       try:
-#         chart = chart + ' ' + category.name[i] + ' '
-#       except:
-#         chart += "   "
-#     chart += " "
-#     i += 1
-#   chart 
-#   return chart
     
